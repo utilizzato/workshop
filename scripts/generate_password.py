3770798f74ed533118ff8c840086fb63844d3750
utilizzato <moshaiov@proton.me>
2025-01-06 15:02:55 +0200

    Create generate_password.py

---
+++ scripts/generate_password.py
@@ -0,0 +1,7 @@
+import random
+import string
+
+def generate_password(length=10):
+    characters = string.ascii_letters + string.digits + "!@#+"
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
